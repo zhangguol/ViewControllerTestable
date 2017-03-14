37c1bd33bea9ce6bcb5d18aac01ea2dd7dc478c5
Boxuan Zhang <zhangboxuan2008@hotmail.com>
2017-03-14 00:16:58 -0700

    Initial Commit

---
+++ ViewControllerTestable/ViewControllerTestable.h
@@ -0,0 +1,19 @@
+//
+//  ViewControllerTestable.h
+//  ViewControllerTestable
+//
+//  Created by Boxuan Zhang on 3/14/17.
+//  Copyright © 2017 Boxuan Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ViewControllerTestable.
+FOUNDATION_EXPORT double ViewControllerTestableVersionNumber;
+
+//! Project version string for ViewControllerTestable.
+FOUNDATION_EXPORT const unsigned char ViewControllerTestableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewControllerTestable/PublicHeader.h>
+
+
