18ae6eb4b2f349ca24a1cd4199c4f7f89302369e
Boxuan Zhang <zhangboxuan2008@hotmail.com>
2017-03-14 00:45:53 -0700

    Initial commit

---
+++ ViewControllerTestable/ViewControllerPresentable.swift
@@ -0,0 +1,9 @@
+//
+//  ViewControllerPresentable.swift
+//  ViewControllerTestable
+//
+//  Created by Boxuan Zhang on 3/14/17.
+//  Copyright © 2017 Boxuan Zhang. All rights reserved.
+//
+
+import Foundation
